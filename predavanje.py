7a729ffa9359d878ddc32cb228fc48e7519586d1
OmerDrazanin <115923917+OmerDrazanin@users.noreply.github.com>
2022-10-23 11:37:44 +0200

    test

---
+++ predavanje.py
@@ -0,0 +1,1067 @@
+# ime = "Omer"
+# prezime = "Dražanin"
+# print(ime + prezime)
+# prvi_broj = 30
+# drugi_broj = 10
+# print (prvi_broj+drugi_broj)
+# prvi_broj = 30
+# drugi_broj = 10
+# print (prvi_broj-drugi_broj)
+# prvi_broj = int(input("unesite neki broj: "))
+# drugi_broj = int(input("unesite neki broj: "))
+# prvi_broj = int(input("unrsite neki broj: "))
+# drugi_broj = int(input("unesite neki broj: "))
+
+# a=int(input("unesite neki broj: "))
+# if a > 2:
+#     print("broj je veci od 2")
+# a== 2:
+#     print("broj je 2")
+# else:
+# print("broj je manji od 2")
+
+# from re import T
+# from socket import J1939_MAX_UNICAST_ADDR
+
+
+# True and True ==> True
+# True or False ==> True
+# False or True ==> True
+# False or False ==> False
+
+# True or True ==> True
+# True or False ==> True
+
+
+# a = int(input("unesite neki broj")
+# b = int(input("unesite neki broj")
+
+# if a > b:
+#     print("prvi broj veci od drugog")
+# elif b > a:
+#     print("drugi broj je veci od drugog")
+# else:
+#     print("brojevi su jrdnaki")
+
+# a=int(input("Unesite neki broj"))
+# if a%2==0:
+#     print("Broj je paran")
+# else:
+#     print("Broj je neparan")
+
+# a = int(input("Unesite neki broj"))
+
+# if a%7==0:
+#     print("Broj je deljiv sa 7")
+# else:
+#     print("Broj nije deljiv sa 7)
+
+# a=float(input("Unesite 1. broj"))
+# b=float(input("unesite 2. broj"))
+# if a>b:
+#     print("a,je vece od b")
+# elif b>a:
+#     print("b je vece od a")
+
+
+
+# prvi_broj = int(input("Unesite neki broj: "))
+# drugi_broj = int(int("unesite neki broj: "))
+# znak=input("unesite neki broj: ")
+
+# if znak=="+":
+#     print("prvi_broj+drugi_broj, "zbir")
+# elif == znak "-"
+# print("prvi_broj-drugi_broj")
+
+
+# m = int(input("unesite mesec"))
+# if m==1:
+#     print("januar")
+# m = int(input("unesite mesec"))
+# if m==2:
+#     print("februar")
+# m = int(input("unesite mesec"))
+# if m==3:
+#     print("mart")
+# m = int(input("unesite mesec"))
+# if m==4:
+#     print("april")
+# m = int(input("unesite mesec"))
+# if m==5:
+#     print("maj")
+# m = int(input("unesite mesec"))
+# if m==6:
+#     print("jun")
+# m = int(input("unesite mesec"))
+# if m==7:
+#     print("jul")
+# m = int(input("unesite mesec"))
+# if m==8:
+#     print("avgust")
+# m = int(input("unesite mesec"))
+# if m==9:
+#     print("septembar")
+# m = int(input("unesite mesec"))
+# if m==10:
+#     print("oktobar")
+# m = int(input("unesite mesec"))
+# if m==11:
+#     print("novembar")
+# m = int(input("unesite mesec"))
+# if m==12:
+#     print("decembar")
+
+# a=int(input("unesite stranicu"))
+# b=int(input("unesite stranicu"))
+# c=int(input("unesite stranicu"))
+# if a==b and a==c:
+#     print("sve stranice su jednake")
+# elif a==b or a==c or b==c:
+#     print("svi kraci su jednaki")
+# else:
+#     print("trougao je raznostranican")
+
+
+# import codeop
+
+
+# ocena = int(input("unesite ocenu ispita (1-5): "))
+# if ocena ==1:
+#     print("ocena je 1, niste prosli ispit")
+# elif ocena ==2:
+#     print("ocena je 2, prosli ste dovoljno")
+# elif ocena ==3:
+#     print("ocena je 3, prosli ste dobro")
+# elif ocena ==4:
+#     print("ocena je 4, prosli ste vrlo dobro")
+# elif ocena ==5:
+#     print("ocena je 5, prosli ste odlican")
+# else:
+#     print("ocena nepostoji")
+
+# a = int(input("unesite doba dana")
+
+# a = int(input("unesite neki broj"))
+# b = int(input("unesite neki broj"))
+# if a==b:
+#     print("print brojevi su jednaki")
+# elif a<b:
+#     print("b je vece od a")
+# elif b<a:
+#     print("a je vece od b")
+# else:
+#     print("pogresan unos")
+
+# a = int(input("unesi prvi broj"))
+# b = int(input("unesi drugi broj"))
+# zbir=a+b
+# proizvod=a*b
+# if zbir>proizvod:
+#     print("zbir je veci od proizvoda")
+
+# d = int(input("unesite dan"))
+# if d==1:
+#     print("ponedeljak")
+# d = int(input("unesite dan"))
+# if d==2:
+
+# for i in range(1,6):
+#     print("brojac")
+
+# for i in range(0,11,2):
+#     print("brojevi su parni", i)
+
+# for i in range(0,11):
+#     print("unosimo policu broj", i)
+
+# for i in range(0,6-1):
+#     print(i)
+# for i in range(6,0-1):
+#     print(i)
+
+# for i in range(100,0,-1):
+#     print(i)
+
+# for i in range(1, 50):
+#     if i =48:
+#         print(i)
+
+# zbir = 0
+# for i in range(0,21,):
+#     zbir=zbir+1
+#     print("nakon broja", i,"zbir je", zbir)
+
+# zad. 7.
+
+# for i in range(1,6):
+#     print(i)
+
+# zad.6
+
+# for i in range(1,4):
+#     print("phyton je super", i)
+
+# zad.8
+
+# for i in range(1,9 -2):
+#     print(i)
+
+# zad.9
+
+
+#a=int(input("unesi neki broj: "))
+# for i in range(a,0,-1):
+#     print(i)
+
+
+# for i in range(0,31):
+#     print("Ja vezbam python")
+
+
+# for i in range(0,100):
+#     print(i)
+
+
+# for i in range(0,96,5):
+#     print(i)
+
+
+# for i in range(0,6):
+#     print(i**2)
+
+
+# for i in range(0,6):
+#     if i % 3 != 0:
+#         print(i, end=" ")
+# for i in range(0,18):
+#     if i % 3 == 0:
+#         print(i, end=" ")
+
+
+# for i in range(0,18):
+#     if i % 2!=0:
+#         print(i, end=" ")
+
+
+
+# a = int(input("unesi neki broj"))
+
+# while a<20:
+#     print("broj je manji od 20")
+#     break
+
+
+# lozinka = int(input("unesite lozinku"))
+
+# while != 'okismoki123':
+#     print("unesite lozinku")
+#     if lozinka == 'okismoki123':
+#         print("uneta lozinka je tacna")
+#         else:
+#             print("uneta lozinka nije tacn, pokusajte ponovo! ")
+
+
+
+# print("igra pogađanja brojeva")
+# print("za prekid igre unesite 0")
+
+# pogodak=20
+# while True:
+#     broj=int(input("unesi neki broj: "))
+#     if broj== pogodak:
+#         print("pogodili ste broj! ")
+#     elif broj == 0:
+#         break
+#     elif broj > pogodak:
+#         print("pokusajte manji broj")
+#     elif broj < pogodak:
+#         print("pokusajte veci broj")
+
+
+
+# G=int(input("unesite godine vaseg junaka"))
+# R=int(input("unesite godinu"))
+# print(G-R, "vas junak je rodjen")
+
+
+
+
+# sijelica=int(input("unesite neki broj")):
+# if sijelica<=60:
+#     print("sijelica pregorela")
+# else sijelica>
+
+
+
+
+
+# voznja=int(input("unesite godine vaseg junaka : "))
+# if voznja <8:
+#     print("voznja je džabe")
+# elif voznja >8 and voznja>18:
+#     print("voznja kosta 200din")
+# elif voznja >20 and voznja>65:
+#     print("voznja je 500din")
+# elif voznja <65:
+#     ("voznja je 100din")
+# voce = ["cherry", "banana,", "apple"]
+# print(voce[0])
+# items = [43, 56.78, "Kadir", True, False]
+# print(items)
+
+# items = [43, 56.78, "Kadir", True, False]
+# for x in items:
+#     print(x)
+
+
+# imena = ["kadir","kadir","kerim"]
+# print(len(imena))
+
+
+ 
+# imena = ["kadir","kadir","kerim"]
+# i = 0
+# for x in imena:
+#     i +=1
+#     print(i)
+
+
+
+
+# oki = 0
+# lista = [1, 2, 3, 5, 9]
+# for i in lista:
+#     oki += i
+
+
+
+# oki = 1
+# lista = [1, 2, 3, 5, 9]
+# for i in lista:
+#     oki *= i
+#     print(oki)
+
+
+# list1 = [10, 20, 1, 45, 99]
+# najmanji = min(list1)
+# print("najmanji broj je: ", najmanji)
+
+
+
+
+# from os import remove
+
+
+# predmeti=['ja', 'ti', 'on', 'ona', 'ono', 'mi', 'vi']
+# predmeti.remove('ja')
+# print(predmeti)
+
+
+
+# predmeti=['ja', 'ti', 'on', 'ona', 'ono', 'mi', 'vi']
+# print(len(predmeti))
+
+
+# thisset = {"apple", "banana", "cherry", "apple"}
+# print(thisset)
+
+
+
+
+
+
+# thisset = {'apple', 'banana', 'cherry'}
+# thisset.add = {'apple1', 'banana1', 'cherry1'}
+# thisset.update  {}
+
+
+
+# thisdict ={
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964}
+# print(thisdict["brand"])
+
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(thisdict)
+
+
+
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.keys()
+
+# print(x) #before the change
+
+# car["color"] = "white"
+
+# print(x) #after the change
+
+
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# car["year"] = 1978
+# print(car)
+
+
+
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# car["brand"] = "konj"
+# car["model"] = "emin"
+# car["year"] = "2010"
+#print (car)
+
+
+
+
+
+# thislist = [1,2,3,4,5,6,7,8,9,10]
+# zbir=0
+# for x in thislist:
+#     zbir += i
+
+
+
+# thislist = [1,2,3,4,5,6,7,8,9,10]
+# proizvod=1
+# for i in thislist:
+#     proizvod *= i
+# print(proizvod)
+
+
+
+# x=('blue','green','yellow','orange','brown')
+# y=list(x)
+# y[3]='brown'
+# x=tuple(y)
+# print(x)
+
+
+
+
+
+# thistuple = ('1',)
+# print(thistuple.index('1'))
+
+
+
+# lista=['kadir','ibrahim','hamza','kadir']
+# print(list(set(lista)))
+
+
+
+
+# lista=['kadir','ibrahim','hamza','kadir']
+# print(set(('car1','car2',)))
+
+
+
+
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+
+# x = car.keys()
+
+# print(x) #before the change
+
+# car["color"] = "white"
+
+# print(x) #after the change
+# if 'model' in car:
+#     print('u njemu se nalazi model')
+
+
+
+
+
+
+# car = {
+# "brand": "Ford",
+# "model": "Mustang",
+# "year": 1964
+# }
+# # for x in car.keys():
+# #   print(x)
+# # for x in car.values():
+# #   print(x)
+# for x, y in car.items():
+#   print(x, y)
+
+
+
+
+# #1
+# lista =[1,2,3,4,5,6,7,8,9,9999]
+# proizvod =1
+# for i in lista:
+#     proizvod+= i
+#     print(proizvod)
+
+
+
+
+# #2
+# lista =[1,2,3,4,5,6,7,8,9]
+# lista1 =[1,2,3,45,67,34575,1]
+# for i in lista:
+#     for j in lista1:
+#         if i ==j:
+#             print("true")
+#         else:
+#            print("false")
+
+
+
+
+
+#3
+# neznam
+
+
+
+
+ #4
+# x=('ja','ti','on','ona','ono')
+# y=list(x)
+# y.remove('ja')
+# y.remove('ti')
+# y.append('mi')
+# x=tuple(y)
+# print(x)
+
+
+
+# #5
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# for x in thisdict.keys():
+#   print(x)
+# for x in thisdict.values():
+#   print(x)
+
+
+
+
+ #6
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# car.pop("brand")
+# car[colors]="blue"
+# car[motor]="v12"
+# print(car)
+
+
+
+
+
+
+
+ #7
+# x={'ja','ti','on','ona','ono'}
+# y=list(x)
+# y.clear()
+# x=set(y)
+# print(x)
+
+
+
+
+
+# a=int(input('unesite neki broj: '))
+# b = []
+# for i in range(1,a):
+#     if a % i == 0:
+#         b.append(i)
+# print(b)
+
+
+
+
+# lista1=['ja','ti','on','ona','ono']
+# n=0
+# b=[]
+# for i in range(len(lista1)):
+#     for j in lista1[i]:
+#         if 'a' in lista1[i] or 'e' in lista1[i] or 'i' in lista1
+
+
+
+
+# def hello():
+#     print("hello world")
+
+
+# hello()
+
+
+
+
+
+
+
+# def ime_prezime():
+#     print("omer drazanin")
+
+
+# ime_prezime()
+
+
+
+
+
+# def my_function(fname):
+#   print(fname + " Refsnes")
+
+# my_function("Emil")
+# my_function("Tobias")
+# my_function("Linus")
+
+
+
+
+
+# def ime_prezime(ime, prezime):
+#     print(ime+prezime)
+
+
+# ime_prezime("omer", "drazanin")
+
+
+
+
+
+
+
+
+
+# def greet():
+#     print("hi there")
+#     print("wlcome aboard")
+
+
+# greet()
+
+
+
+
+
+
+
+
+
+
+# def greet(ime, name):
+#     print(f"hi {ime} {name}")
+#     print("wlcome aboard")
+
+
+# greet("oki", "smoki")
+
+
+
+
+
+
+
+# def func1(a,b):
+#     sum = a + b
+#     sub = a - b
+#     mul = a * b
+#     print(f"sabrani brojevi su {sum}, oduzeti brojevi su {sub}, pomnozeni {mul}")
+
+
+
+# a = int(input("unesite neki broj"))
+# b = int(input("unesite neki broj"))
+# func1(a,b)
+
+# def omer(a,b,c):
+#     if a>b and a>c:
+#         print(a, "je najveci broj")
+#     elif b>c and b>a:
+#         print(b, "je najveci broj")
+#     else:
+#         print(c, "je najveci broj")
+    
+# a=int(input("unesi 1. broj: "))
+# b=int(input("unesi 2. broj: "))
+# c=int(input("unesi 3. broj: "))
+
+# omer (a,b,c)
+
+
+
+
+
+
+# def aisa(a):
+#     if a%2==0:
+#         print("broj je paran")
+#     elif a%2==1:
+#         print("broj je neparan")
+#     else:
+#         print("broj je 0")
+
+
+
+# a=int(input("unesite neki broj"))
+# aisa(a)
+
+
+
+
+
+
+
+
+# def ime_funkcije(a,b):
+#     sum = a + b
+#     print(sum)
+
+
+# ime_funkcije(1,2)
+
+
+
+
+
+
+
+
+
+
+# def greet(ime, name):
+#     print(f"hi {ime} {name}")
+#     print("wlcome aboard")
+
+
+# greet("oki", "smoki")
+
+
+
+
+
+
+
+
+# def aisa(a):
+#     if a%2==0:
+#         print("broj je paran")
+#     elif a%2==1:
+#         print("broj je neparan")
+#     else:
+#         print("broj je 0")
+
+
+
+# a=int(input("unesite neki broj"))
+# aisa(a)
+
+
+
+
+
+
+
+
+# def max_of_two( x, y ):
+#     if x > y:
+#         return x
+#     return y
+# def max_of_three( x, y, z ):
+#     return max_of_two( x, max_of_two( y, z ) )
+# print(max_of_three(3, 6, -5))
+
+
+
+
+
+
+
+
+# lista[]
+# for i in range(5):
+#     b=int(input("")
+
+
+
+
+
+
+
+# def suljejman(a):
+#     d={"suljejman_konj":0, "tarik_konj":0}
+#     for b in a:
+#         if b.isupper():
+#            d["suljejman_konj"]+=1
+#         elif b.islower():
+#            d["tarik_konj"]+=1
+#         else:
+#            pass
+#     print ("Original String : ", a)
+#     print ("No. of Upper case characters : ", d["suljejman_konj"])
+#     print ("No. of Lower case Characters : ", d["tarik_konj"])
+
+# suljejman('The quick Brown Fox')
+
+
+
+
+
+
+
+
+# def aisa(j):
+#   x = []
+#   for a in j:
+#     if a not in x:
+#       x.append(a)
+#   return x
+
+# print(aisa([1,2,3,3,3,3,4,5])) 
+
+
+
+
+
+
+
+
+
+
+
+# int(input("unesite neki broj"))
+# def func1(a,b,c):
+#     sum = a + b
+#     sub = a + c
+#     mul = b + c
+# print(func1)
+
+
+
+# func1(7,5,8)
+
+
+
+
+
+
+
+# for i in range(11,21):
+#     print(i)
+
+
+
+
+
+
+
+
+
+# a = int(input("unesite neki broj :"))
+# def fizbuzz(a):
+#    if a%3 ==0:
+#         print("fiz")
+#      elif a%3 and a%5 ==0:
+#         print(fizbuzz)
+#    elif a%5 ==0:
+#         print("buzz")
+#    else:
+#         print("nema pcela")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# s = input("unesite YES or NO :")
+# def shootdown(s):
+#     if s == "YES":
+#         print("shootdown")
+#     elif s == "NO":
+#         print("shootdown aboard")
+#     else:
+#         print("sorry")
+
+
+# shootdown(s)
+
+
+
+
+
+
+
+
+
+
+
+
+# int(input("unesite neki broj :"))
+# def brzina_vozila(a):
+#     if a==70:
+#         print("to je vasa prosecna brzina")
+#     elif a>
+
+
+
+
+
+
+
+
+
+
+# s = input("unesite glup or pametan :")
+# def shootdown(s):
+#     if s == "glup":
+#         print("shootdown")
+#     elif s == "pametan":
+#         print("shootdown aboard")
+#     else:
+#         print("sorry")
+
+
+# shootdown(s)
+
+
+# #1.
+# def func(a):
+#     if a ==0:
+#         print('broj je nula')
+#     else:
+#         print('broj nije nula')
+
+
+
+
+# func(0)
+
+
+
+
+
+
+# # 2.
+# a = int(input("unesite neki broj :"))
+# def deljenje(a):
+#    if a%5==0:
+#         print("deljivo je sa pet")
+#    else:
+#         print("nije deljivo sa pet")
+# deljenje(a)
+
+
+
+
+
+
+
+
+
+# for i in range(3,11):
+#     if i%3==0:
+#         print(i)
+
+
+
+
+
+
+
+
+
+
+# a = random.randint(1,23)
+# print(a)
+
+
+
+
+
+
+
+
+# a = random.uniform(10,99.99999)
+# print(a)
+
+
+
+
+
+
+
+# a = random.randint(10,99)
+# b = random.randint(10,99)
+# print(a,b)
+
+
+
+
+
+
+# class osoba:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+
+#   def print_name(self) -> str:
+#     print(self.name)
+
+
+# nova_osoba = osoba("omer", "12")
+# nova_osoba.print_name()
+
+
+
+
+
+
+
+
+
